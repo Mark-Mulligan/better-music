9d744ae7df2791ed6911d0f6d9dd84b781faf9da
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-10-26 21:00:53 -0500

    add more functionallity to custom select

--- components/ui/Select.js
+++ components/ui/Select.js
@@ -3,16 +3,23 @@ import styles from './Select.module.css';
 
 const Select = ({ options }) => {
   const [selectedOptions, setSelectOptions] = useState([]);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleOptionClick = (selectedItem) => {
     console.log([...selectedOptions, selectedItem]);
     setSelectOptions([...selectedOptions, selectedItem]);
   };
 
+  const handleSelectInputClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <form className={styles.select}>
-      <div className={styles.selectInput}></div>
-      <ul>
+      <div className={styles.selectInput} onClick={handleSelectInputClick}>
+        <span>Genres</span>
+      </div>
+      <ul style={{ height: menuOpen ? 'auto' : 0, overflow: menuOpen ? 'auto' : 'hidden' }}>
         {options.map((option) => (
           <li onClick={() => handleOptionClick(option)} key={option.value}>
             {option.label}
