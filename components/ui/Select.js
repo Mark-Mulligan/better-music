34501c01246f24ff283ebe15061d10ecf9bead8b
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-10-27 23:52:05 -0500

    added chips to select

--- components/ui/Select.js
+++ components/ui/Select.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './Select.module.css';
 
 const Select = ({ options }) => {
@@ -31,7 +33,10 @@ const Select = ({ options }) => {
       <div className={styles.selectInput} onClick={handleSelectInputClick}>
         <span className={selectedOptions.length > 0 ? styles.filledSelectLabel : styles.emptySelectLabel}>Genres</span>
         {selectedOptions.map((item) => (
-          <span key={item.label}>{item.label}</span>
+          <div className={styles.chip} key={item.label}>
+            <span>{item.label}</span>
+            <FontAwesomeIcon className={styles.closeIcon} icon={faTimes} size="sm" />{' '}
+          </div>
         ))}
       </div>
       <ul style={{ height: menuOpen ? 'auto' : 0, overflow: menuOpen ? 'auto' : 'hidden' }}>
