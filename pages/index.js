432f2d0d7fcd7f6cef8632494e027d7e34323cd4
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-11-04 12:21:42 -0500

    added new box shadow to artist cards

--- pages/index.js
+++ pages/index.js
@@ -42,27 +42,25 @@ export default function Home({ artistList }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className={styles.specialBackground}>
-          <section className={styles.heroSection}>
-            <h1>Better Music</h1>
-          </section>
+      <main className={styles.mainBody}>
+        <section className={styles.heroSection}>
+          <h1>Better Music</h1>
+        </section>
 
-          <section className={styles.aboutSection}>
-            <h2>About</h2>
-            <p>
-              In today&#39;s world, we have access to more music than ever before. Even so, it can be hard to find new
-              and interesting music. The goal of this website is to give you a selection artists to help you on your
-              quest for better music.
-            </p>
-          </section>
-        </div>
+        <section className={styles.aboutSection}>
+          <h2>About</h2>
+          <p>
+            In today&#39;s world, we have access to more music than ever before. Even so, it can be hard to find new and
+            interesting music. The goal of this website is to give you a selection artists to help you on your quest for
+            better music.
+          </p>
+        </section>
 
         <section className={styles.artistSection}>
           <h2>Artists</h2>
 
           <div className={styles.searchContainer}>
-            <div className="relative">
+            <div className={styles.inputCol}>
               <div className={styles.customInputWrapper}>
                 <CustomInput
                   id="artistSearch"
